68e0a95b9bda9979c29533f47dd2d976371a3db7
lili14gon <liligh14@gmail.com>
2019-07-09 10:49:55 -0500

    separando por sectores

--- src/data.js
+++ src/data.js
@@ -9,17 +9,24 @@
    NuevoTitulo=object.keys(tituloCountry);
    return NuevoTitulo;
  }
+// const sectorIndicador = (sectorVariado)=>{
+//   let sector = [];
+//   sector=object.keys(sectorVariado);
+//   return sector;
+// }
+
 // Funcion para seleccion un indicador de un pais
-const indicadoresPorPais = (data, pais) => {
-  const ListaNueva = [];
-  let indicator = data[pais].indicators;
-  for (let i = 0; i < indicator.length; i++) {
-    ListaNueva.push(indicator[i]['countryName'] + ' - ' + indicator[i]['indicatorName']);
-  }
-  return ListaNueva;
-}
+// const indicadoresPorPais = (data, pais) => {
+//   const ListaNueva = [];
+//   let indicator = data[pais].indicators;
+//   for (let i = 0; i < indicator.length; i++) {
+//     ListaNueva.push(indicator[i]['countryName'] + ' - ' + indicator[i]['indicatorName']);
+//   }
+//   return ListaNueva;
+// }
+
 // Funcion para s
-const tituloPorPais=(data,pais,tituloCountry)=>{
+const tituloPorPais=(data,pais)=>{
   const titleNew=[];
   let titleCountry = data[pais].indicators;
   for(let l=0;l<titleCountry.length; l++){
@@ -27,9 +34,23 @@ const tituloPorPais=(data,pais,tituloCountry)=>{
   }
   return titleNew;
 }
+//funcion para selecionar un sector
+const indicadoresPorSector =(data,pais,sector)=>{
+  const listaNuevaSector = [];
+  let indicadores = data[pais].indicators;
+     for(let n = 0; n < indicadores.length; n ++){
+         if((indicadores[n]['indicatorCode']).substring(0,2)==sector){
+         listaNuevaSector.push(indicadores[n]['countryName']+indicadores[n]['indicatorName']+indicadores[n]['indicatorCode']);
+      }
+    }
+    return listaNuevaSector;
+  }
+ // debugger
 window.worldbank = {
   arrayCountry: arrayCountry,
   tituloPais:tituloPais,
-  indicadoresPorPais: indicadoresPorPais,
+  // sectorIndicador:sectorIndicador,
+  // indicadoresPorPais: indicadoresPorPais,
   tituloPorPais: tituloPorPais,
+  indicadoresPorSector:indicadoresPorSector
 }
--- src/main.js
+++ src/main.js
@@ -1,81 +1,87 @@
 /* Manejo del DOM */
-const password=document.getElementById("password");
-const seccionOne=document.getElementById("seccion1");
-const seccionTwo=document.getElementById("seccion2");
-const seccionThree=document.getElementById("seccion3");
-const seccionFour=document.getElementById("seccion4");
-const seccionFive=document.getElementById("seccion5");
-const mnsInc=document.getElementById("mnsInc");
-// const btnIngresar=document.getElementById("btnIngresar");
-const header=document.getElementById("header");
-const btnStart=document.getElementById("start");
-const btnCountry=document.getElementById("country");
-const selectCountry=document.getElementById("paises");
-const btnIndicator=document.getElementById("sector");
+const password = document.getElementById("password");
+const seccionOne = document.getElementById("seccion1");
+const seccionTwo = document.getElementById("seccion2");
+const seccionThree = document.getElementById("seccion3");
+const seccionFour = document.getElementById("seccion4");
+const seccionFive = document.getElementById("seccion5");
+const mnsInc = document.getElementById("mnsInc");
+const header = document.getElementById("header");
+const btnStart = document.getElementById("start");
+const btnCountry = document.getElementById("country");
+const selectCountry = document.getElementById("paises");
+const btnIndicator = document.getElementById("sector");
 const ListIndicador = document.getElementById('list');
 const titulo = document.getElementById("titulo");
-const buscar= document.getElementById('buscar');
-const numeroInicial=document.getElementById('year1');
-const numeroFinal=document.getElementById('year2');
-// btnIngresar.addEventListener('click',()=>{
-// if(password.value===''){
-//     mnsInc.innerHTML="ingrese password";
-// }
-// else{
-//     if(password.value==='laboratoria'){
-//         seccionOne.classList.add('hide');
-//         header.classList.remove('hide');
-//         seccionTwo.classList.remove('hide');
-//     }
-//     else{
-//         mnsInc.innerHTML="contraseña incorrecta";
-//     }
-// }
-// password.value="";
-// });
+const selectSectores = document.getElementById("sectores");
+const mostrarIndicador = document.getElementById("indicadoresSector");
 
-btnCountry.addEventListener('click',()=>{
-    seccionTwo.classList.add('hide');
-    seccionThree.classList.remove('hide');
-});
-btnStart.addEventListener('click',()=>{
-    seccionTwo.classList.remove('hide');
-    seccionThree.classList.add('hide');
-    });
-
-
-const pintarIndicadoresPorPais = (arrayIndicadores)=>{
-    let string =`<option disabled selected> Seleccione un indicador </option>`;
-    for(let i=0; i<arrayIndicadores.length;i++){
-      string +=`<option value="${arrayIndicadores[i]}">${arrayIndicadores[i]}</option>`
+const pintarIndicadoresPorSectoryPais = (arrayIndicadoresxSector) => {
+    let string = `<p id="indicadoresSector"></p>`;
+    for (let i = 0; i < arrayIndicadoresxSector.length; i++) {
+        string += `<p value="${arrayIndicadoresxSector[i]}">${arrayIndicadoresxSector[i]}</p>`
     }
     return string;
 };
-const pintarNommbresPorPais=(arrayTitle)=>{
-    let stringTitulo=`<h1 id="titulo"></h1>`;
-    for(let l=0;l<arrayTitle.length;l++){
-        stringTitulo =`INDICADORES DE ${arrayTitle[l]}`.toUpperCase();
+btnIngresar.addEventListener('click', () => {
+    // if (password.value === '') {
+    //     mnsInc.innerHTML = "ingrese password";
+    // }
+    // else {
+    //     if (password.value === 'laboratoria') {
+            seccionOne.classList.add('hide');
+            header.classList.remove('hide');
+            seccionTwo.classList.remove('hide');
+    //     }
+    //     else {
+    //         mnsInc.innerHTML = "contraseña incorrecta";
+    //     }
+    // }
+    // password.value = "";
+});
+
+btnCountry.addEventListener('click', () => {
+    seccionTwo.classList.add('hide');
+    seccionThree.classList.remove('hide');
+});
+btnStart.addEventListener('click', () => {
+    seccionTwo.classList.remove('hide');
+    seccionThree.classList.add('hide');
+});
+const pintarIndicadoresPorPais = (arrayIndicadores) => {
+    let string = `<option disabled selected> Seleccione un indicador </option>`;
+    for (let i = 0; i < arrayIndicadores.length; i++) {
+        string += `<option value="${arrayIndicadores[i]}">${arrayIndicadores[i]}</option>`
+    }
+    return string;
+};
+const pintarNommbresPorPais = (arrayTitle) => {
+    let stringTitulo = `<h1 id="titulo"></h1>`;
+    for (let l = 0; l < arrayTitle.length; l++) {
+        stringTitulo = `INDICADORES DE ${arrayTitle[l]}`.toUpperCase();
     }
     return stringTitulo;
 }
+let paisSeleccionado;
 selectCountry.addEventListener('click', (event) => {
-    const paisSeleccionado = event.target.value;
-    if(paisSeleccionado !== '') {
-    seccionThree.classList.add('hide');
-    seccionFour.classList.remove('hide');
-    titulo.innerHTML= pintarNommbresPorPais(worldbank.tituloPorPais(WORLDBANK, paisSeleccionado));
-    ListIndicador.innerHTML = pintarIndicadoresPorPais(worldbank.indicadoresPorPais(WORLDBANK, paisSeleccionado));
-   }
+    paisSeleccionado = event.target.value;
+    if (paisSeleccionado !== '') {
+        seccionThree.classList.add('hide');
+        seccionFour.classList.remove('hide');
+        titulo.innerHTML = pintarNommbresPorPais(worldbank.tituloPorPais(WORLDBANK, paisSeleccionado));
+    }
 })
-btnIndicator.addEventListener('click',()=>{
+selectSectores.addEventListener('click', (event) => {
+    const sectorSeleccionado = event.target.value;
+    if (sectorSeleccionado !== '') {
+        mostrarIndicador.innerHTML = pintarIndicadoresPorSectoryPais(worldbank.indicadoresPorSector(WORLDBANK,paisSeleccionado,sectorSeleccionado));
+        console.log(sectorSeleccionado);
+    }
+});
+btnIndicator.addEventListener('click', () => {
     seccionFour.classList.add('hide');
     seccionFive.classList.remove('hide');
 });
-buscar .addEventListener('click',(event)=>{
-
-
+buscar.addEventListener('click', (event) => {
     event.preventDefault();
-    console.log(list.value);
-    console.log(numeroInicial.value);
-    console.log(numeroFinal.value);
 })
