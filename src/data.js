13bf837b6f14cc9047f774df83695f9ff3af0ad3
lili14gon <liligh14@gmail.com>
2019-07-09 19:57:40 -0500

    corriendo el cambio original

--- src/data.js
+++ src/data.js
@@ -9,7 +9,28 @@
    NuevoTitulo=object.keys(tituloCountry);
    return NuevoTitulo;
  }
-// Funcion para ver pais seleccionado
+// Funcion para seleccion un indicador de un pais
+const indicadoresPorPais = (data, pais) => {
+  const ListaNueva = [];
+  let indicator = data[pais].indicators;
+  for (let i = 0; i < indicator.length; i++) {
+    ListaNueva.push(indicator[i]['countryName'] + ' - ' + indicator[i]['indicatorName']);
+  }
+  return ListaNueva;
+}
+// Funcion para mostrar los porcentajes de un indicador por los años seleccionados
+const selectRangoYear = (numeroInicial, numeroFinal, objYear) => {
+  let rango = [];
+  const arrYears = Object.keys(objYear);
+  const arrValue =Object.values(objYear);
+  for (let i = 0; i <= arrYears.length; i++) {
+      if(arrYears[i] >= numeroInicial && arrYears[i] <= numeroFinal){
+          rango.push([arrYears[i],arrValue[i]]);
+      } 
+  }
+  return rango;
+}
+// Funcion para mostrar el titulo del pais seleccionar
 const tituloPorPais=(data,pais)=>{
   const titleNew=[];
   let titleCountry = data[pais].indicators;
