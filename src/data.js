dc2eeca2111ba197f3829d4b2d3b15ac01daaed6
lili14gon <liligh14@gmail.com>
2019-07-12 20:59:49 -0500

    creando  funciones de obtener data

--- src/data.js
+++ src/data.js
@@ -1,23 +1,27 @@
 /* Manejo de data */
-const arrayCountry =(pais)=>{
-  let country = [];
-  country = object.keys(pais);
-  return country;
-}
-const tituloPais =(tituloCountry)=>{
-  let NuevoTitulo=[];
-  NuevoTitulo=object.keys(tituloCountry);
-  return NuevoTitulo;
-}
+// const arrayCountry =(pais)=>{
+//   let country = [];
+//   country = object.keys(pais);
+//   return country;
+// }
+// const tituloPais =(tituloCountry)=>{
+//   let NuevoTitulo=[];
+//   NuevoTitulo=object.keys(tituloCountry);
+//   return NuevoTitulo;
+// }
+
+
+
 // Funcion para seleccion un indicador de un pais
-const indicadoresPorPais = (data, pais) => {
- const ListaNueva = [];
- let indicator = data[pais].indicators;
- for (let i = 0; i < indicator.length; i++) {
-   ListaNueva.push(indicator[i]['countryName'] + ' - ' + indicator[i]['indicatorName']);
- }
- return ListaNueva;
-}
+// const indicadoresPorPais = (data, pais) => {
+//  const ListaNueva = [];
+//  let indicator = data[pais].indicators;
+//  for (let i = 0; i < indicator.length; i++) {
+//    ListaNueva.push(indicator[i]['countryName'] + ' - ' + indicator[i]['indicatorName']);
+//  }
+//  return ListaNueva;
+// }
+
 //funcion para selecionar un sector
 const indicadoresPorSector =(data,pais,sector)=>{
   const listaNuevaSector = [];
@@ -31,10 +35,10 @@ const indicadoresPorSector =(data,pais,sector)=>{
   }
  // debugger
 // Funcion para mostrar los porcentajes de un indicador por los años seleccionados
-const selectRangoYear = (numeroInicial, numeroFinal, objYear) => {
+const selectRangoYear = (numeroInicial, numeroFinal, datoIndicador) => {
   let rango = [];
-  const arrYears = Object.keys(objYear);
-  const arrValue =Object.values(objYear);
+  const arrYears = Object.keys(datoIndicador);
+  const arrValue =Object.values(datoIndicador);
   for (let i = 0; i <= arrYears.length; i++) {
       if(arrYears[i] >= numeroInicial && arrYears[i] <= numeroFinal){
           rango.push([arrYears[i],arrValue[i]]);
@@ -42,6 +46,21 @@ const selectRangoYear = (numeroInicial, numeroFinal, objYear) => {
   }
   return rango;
 }
+
+
+const indicadoresPorSectorconData =(data,pais,sector)=>{
+  const listaNuevaAnioyPorc = [];
+  let indicadores = data[pais].indicators;
+     for(let n = 0; n < indicadores.length; n ++){
+         if((indicadores[n]['indicatorCode']).substring(0,2)==sector){
+         listaNuevaSector.push(indicadores[n]["data"]);
+      }
+    }
+    return listaNuevaAnioyPorc;
+  }
+
+
+
 // Funcion para mostrar el titulo del pais seleccionar
 const tituloPorPais=(data,pais)=>{
   const titleNew=[];
@@ -53,9 +72,10 @@ const tituloPorPais=(data,pais)=>{
 }
 window.worldbank = {
   selectRangoYear :selectRangoYear,
-  arrayCountry: arrayCountry,
-  tituloPais:tituloPais,
-  indicadoresPorPais: indicadoresPorPais,
+  // arrayCountry: arrayCountry,
+  // tituloPais:tituloPais,
+  //indicadoresPorPais: indicadoresPorPais,
   tituloPorPais: tituloPorPais,
   indicadoresPorSector:indicadoresPorSector,
+  indicadoresPorSectorconData:indicadoresPorSectorconData,
 }
--- src/main.js
+++ src/main.js
@@ -13,15 +13,20 @@ const titulo = document.getElementById("titulo");
 const buscar = document.getElementById('buscar');
 const numeroInicial = document.getElementById('year1');
 const numeroFinal = document.getElementById('year2');
-const resultadoGrafico = document.getElementById('chart_div');
+//const resultadoGrafico = document.getElementById('chart_div');
 const selectSectores = document.getElementById("sectores");
 const mostrarIndicador = document.getElementById("indicadoresSector");
+
+const mostrarAnioPorcentaje=document.getElementById("aniopor");
+
 menuLogin.addEventListener('click', () => {
     document.getElementById("header").classList.add('hide');
     document.getElementById("seccion1").classList.remove('hide');
     document.getElementById("seccion2").classList.add('hide');
     document.getElementById("seccion3").classList.add('hide');
-    document.getElementById("seccion5").classList.add('hide');
+    // document.getElementById("seccion5").classList.add('hide');
+
+    document.getElementById("seccion6").classList.add("hide");
 })
 btnIngresar.addEventListener('click', () => {
     // if(password.value===''){
@@ -42,13 +47,17 @@ btnIngresar.addEventListener('click', () => {
 menuInicio.addEventListener('click', () => {
     document.getElementById("seccion2").classList.remove('hide');
     document.getElementById("seccion3").classList.add('hide');
-    document.getElementById("seccion5").classList.add('hide');
+    // document.getElementById("seccion5").classList.add('hide');
+
+    document.getElementById("seccion6").classList.add("hide");
 });
 menuCountry.addEventListener('click', () => {
     document.getElementById("seccion2").classList.add('hide');
     document.getElementById("seccion3").classList.remove('hide');
     document.getElementById("seccion4").classList.add('hide');
-    document.getElementById("seccion5").classList.add('hide');
+    // document.getElementById("seccion5").classList.add('hide');
+
+    document.getElementById("seccion6").classList.add("hide");
 });
 const pintarNommbresPorPais = (arrayTitle) => {
     let stringTitulo = `<h1 id="titulo"></h1>`;
@@ -67,32 +76,62 @@ selectCountry.addEventListener('click', (event) => {
         document.getElementById("seccion3").classList.add('hide');
         document.getElementById("seccion4").classList.remove('hide');
         titulo.innerHTML = pintarNommbresPorPais(worldbank.tituloPorPais(WORLDBANK, paisSeleccionado));
-        ListIndicador.innerHTML = pintarIndicadoresPorPais(worldbank.indicadoresPorPais(WORLDBANK, paisSeleccionado));
+        // ListIndicador.innerHTML = pintarIndicadoresPorPais(worldbank.indicadoresPorPais(WORLDBANK, paisSeleccionado));
     }
 });
 
-const pintarIndicadoresPorPais = (arrayIndicadores) => {
-    let string = `<option disabled selected> Seleccione un indicador </option>`;
-    for (let i = 0; i < arrayIndicadores.length; i++) {
-        string += `<option id=${i} value="${i}">${arrayIndicadores[i]}</option>`
+// const pintarIndicadoresPorPais = (arrayIndicadores) => {
+//     let string = `<option disabled selected> Seleccione un indicador </option>`;
+//     for (let i = 0; i < arrayIndicadores.length; i++) {
+//         string += `<option id=${i} value="${i}">${i+' '+arrayIndicadores[i]}</option>`
+//     }
+//     return string;
+// };
+
+//muestra opciones por cada sector seleccionado
+ const pintarIndicadoresPorSectoryPais = (arrayIndicadoresxSector) => {
+    let string = `<ul id="indicadoresSector"></ul>`;
+    for (let i = 0; i < arrayIndicadoresxSector.length; i++) {
+        string += `<ul id=${i} value="${i}">${i+' '+arrayIndicadoresxSector[i]}</ul>`
     }
     return string;
 };
 
-//muestra opciones por cada sector seleccionado
-const pintarIndicadoresPorSectoryPais = (arrayIndicadoresxSector) => {
-    let string = `<ul id="indicadoresSector"></ul>`;
-    for (let i = 0; i < arrayIndicadoresxSector.length; i++) {
-        string += `<ul id="indicadoresSector" value="${arrayIndicadoresxSector[i]}">${arrayIndicadoresxSector[i]}</ul>`
-    }
-    return string;
-};
+
+
+//muestra los anios
+const pintarAnioporRango = (IndicadoresxSectorAnio) =>{
+let string = `<fieldset id="aniopor"></fieldset>`;
+for (let i = 0; i < IndicadoresxSectorAnio.length; i++) {
+    string += `<ul id=${i} value="${i}">${i+' '+IndicadoresxSectorAnio[i]}</ul>`
+}
+return string;
+}
+//--------------------------------------------
+let sectorSeleccionado;
 selectSectores.addEventListener('click', (event) => {
-    const sectorSeleccionado = event.target.value;
+    sectorSeleccionado = event.target.value;
     if (sectorSeleccionado !== '') {
         mostrarIndicador.innerHTML = pintarIndicadoresPorSectoryPais(worldbank.indicadoresPorSector(WORLDBANK,paisSeleccionado,sectorSeleccionado));
     }
 });
+
+//----------------------------------------------
+//
+buscar.addEventListener('click',(event)=>{
+    event.preventDefault();
+const indicadorSeleccionado= event.target.value;
+if(indicadorSeleccionado!==''){
+
+document.getElementById("seccion4").classList.add('hide');
+//document.getElementById("seccion5").classList.add('hide');
+document.getElementById("seccion6").classList.remove('hide');
+mostrarAnioPorcentaje.innerHTML = pintarAnioporRango(worldbank.selectRangoYear(numeroInicial,numeroFinal,indicadoresPorSectorconData(WORLDBANK,paisSeleccionado,sectorSeleccionado)));
+}
+});
+
+
+
 //muestra el contenido del indicador seleccionado
 const listaDeIndicadores =  document.getElementById('list');
 listaDeIndicadores.addEventListener('change', (event) => {
@@ -100,37 +139,37 @@ listaDeIndicadores.addEventListener('change', (event) => {
    console.log(selectIndicador);
 });
 
-const grafico=()=>{
-    let arrayGrafico= new Array(Object.entries( selectIndicador));
-    let rango = arrayGrafico[0];
-    rango.forEach((elemento,indice)=>{
-      elemento[0]=(rango[indice][0]);
-      elemento[1]=parseInt(rango[indice][1]);  
-    });
-    let data = new google.visualization.DataTable();
-    data.addColumn('string', 'X');
-    data.addColumn('number', '% ');
-    data.addRows(rango);
-    const options = {
-      hAxis: {
-        title: 'AÑOS'
-      },
-      vAxis: {
-        title: 'PORCENTAJE'
-      }
-    };
-    var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
-    chart.draw(data, options);
-  }
-menuGraphic.addEventListener('click', () => {
-    document.getElementById("seccion4").classList.add('hide');
-    document.getElementById("seccion5").classList.remove('hide');
-});
-buscar.addEventListener('click', (event) => {
-    event.preventDefault();
-    document.getElementById("seccion4").classList.add('hide');
-    document.getElementById("seccion5").classList.remove('hide');
-    grafico();
-    console.log(selectRangoYear(numeroInicial.value, numeroFinal.value, selectIndicador))
-}); 
+// const grafico=()=>{
+//     let arrayGrafico= new Array(Object.entries( selectIndicador));
+//     let rango = arrayGrafico[0];
+//     rango.forEach((elemento,indice)=>{
+//       elemento[0]=(rango[indice][0]);
+//       elemento[1]=parseInt(rango[indice][1]);  
+//     });
+//     let data = new google.visualization.DataTable();
+//     data.addColumn('string', 'X');
+//     data.addColumn('number', '% ');
+//     data.addRows(rango);
+//     const options = {
+//       hAxis: {
+//         title: 'AÑOS'
+//       },
+//       vAxis: {
+//         title: 'PORCENTAJE'
+//       }
+//     };
+//     var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+//     chart.draw(data, options);
+//   }
+// menuGraphic.addEventListener('click', () => {
+//     document.getElementById("seccion4").classList.add('hide');
+//     document.getElementById("seccion5").classList.remove('hide');
+// });
+// buscar.addEventListener('click', (event) => {
+//     event.preventDefault();
+//     document.getElementById("seccion4").classList.add('hide');
+//     document.getElementById("seccion5").classList.remove('hide');
+//     grafico();
+//     console.log(selectRangoYear(numeroInicial.value, numeroFinal.value, selectIndicador))
+// }); 
 
