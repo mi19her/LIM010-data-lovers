2df5eda3cb9b62c5d1b2338d724f5cf3546cbaac
lili14gon <liligh14@gmail.com>
2019-07-15 10:59:24 -0500

    corrigiendo codigo de titulo por indicador´

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 
-google.charts.load('current', { packages: ['corechart', 'bar'] });
+// google.charts.load('current', { packages: ['corechart', 'bar'] });
 const password = document.getElementById("password");
 const mnsInc = document.getElementById("mnsInc");
 const btnIngresar = document.getElementById("btnIngresar");
@@ -19,7 +19,7 @@ const mostrarIndicador = document.getElementById("indicadoresSector");
 const mostrarDatosSector = document.getElementById('mostrarDatosSector');
 const tituloIndicador = document.getElementById("tituloIndicador");
 const promedio = document.getElementById("promedio");
-const muestraPromedio= document.getElementById("muestraPromedio");
+const muestraPromedio = document.getElementById("muestraPromedio");
 const ordenar = document.getElementById("ordenar");
 //funciones para el menu que permite mostrar y ocultar secciones
 menuLogin.addEventListener('click', () => {
@@ -73,26 +73,15 @@ selectCountry.addEventListener('click', (event) => {
         document.getElementById("seccion3").classList.add('hide');
         document.getElementById("seccion4").classList.remove('hide');
         titulo.innerHTML = pintarNommbresPorPais(worldbank.tituloPorPais(WORLDBANK, paisSeleccionado));
-        // ListIndicador.innerHTML = pintarIndicadoresPorPais(worldbank.indicadoresPorPais(WORLDBANK, paisSeleccionado));
+
     }
 });
 //funcion para mostrar el nombre del pais como titulo
-const pintarNommbresPorPais = (arrayTitle) => {
+const pintarNommbresPorPais = (Title) => {
     let stringTitulo = `<h1 id="titulo"></h1>`;
-    for (let l = 0; l < arrayTitle.length; l++) {
-        stringTitulo = `INDICADORES DE ${arrayTitle[l]}`.toUpperCase();
-    }
+    stringTitulo = `INDICADORES DE ${Title}`.toUpperCase();
     return stringTitulo;
 }
-//funcion para mostrar todo el array de indicadores dentro de las opciones del select
-// const pintarIndicadoresPorPais = (arrayIndicadores) => {
-//     let string = `<option disabled selected> Seleccione un indicador </option>`;
-//     for (let i = 0; i < arrayIndicadores.length; i++) {
-//         string += `<option id=${i} value="${i}">${arrayIndicadores[i]}</option>`
-//     }
-//     return string;
-// };
-
 //funcion que validar la seleccion de un sector y jala sus respectivos indicadores
 let sectorSeleccionado;
 selectSectores.addEventListener('click', (event) => {
@@ -107,13 +96,14 @@ const pintarIndicadoresPorSectoryPais = (arrayIndicadoresxSector) => {
     let string2 = '';
     for (let i = 0; i < arrayIndicadoresxSector.length; i++) {
         // const indiceOriginal = arrayIndicadoresxSector[i].substring(0,3) 
-        string2 += `<li id=${i} value="${arrayIndicadoresxSector[i]}">${i + '-' + arrayIndicadoresxSector[i]}</li>`
+
+        //string2 += `<li id=${i} value="${arrayIndicadoresxSector[i]}">${i + '-' + arrayIndicadoresxSector[i]}</li>`
+        string2 += `<li id=${i} value="${arrayIndicadoresxSector[i]}">${arrayIndicadoresxSector[i]}</li>`
     }
     return string2;
 };
-//muestra los objetos de datos y ordena en una tabla
-let porcentaje=[];
-let years=[];
+let porcentaje = [];
+let years = [];
 const pintarDatosxSectorxIndicadores = (obj) => {
     years = obj;
     let stringDatosSector = " ";
@@ -123,32 +113,34 @@ const pintarDatosxSectorxIndicadores = (obj) => {
         if (years[i] !== "") {
             stringDatosSector +=
                 ` <tr><td> ${i} </td>
-        <td>${ years[i].toFixed(2)}</td></tr>`;
+            <td>${ years[i].toFixed(2)}</td></tr>`;
         }
     }
     return stringDatosSector;
 };
+
 //funcion para mostrar los datos del indicador seleccionado
 mostrarIndicador.addEventListener('click', (event) => {
+    let indice = event.target.id;
     document.getElementById("seccion4").classList.add('hide');
     document.getElementById("seccion5").classList.remove('hide');
     const indicadoresData = worldbank.datosPaisSector(WORLDBANK, paisSeleccionado, sectorSeleccionado);
     mostrarDatosSector.innerHTML = pintarDatosxSectorxIndicadores(indicadoresData[event.target.id]);
-    //funcion para jalar el nombre del indicador seleccionado
-    const pintarTituloPorIndicador = (arrayTitle) => {
-        let stringTitulo = `<h3 id="tituloIndicador"></h3>`;
-        stringTitulo = `${arrayTitle}`;
-        tituloIndicador.innerHTML = stringTitulo.toUpperCase();
-    }
-    pintarTituloPorIndicador(worldbank.tituloPorIndicador(WORLDBANK, paisSeleccionado, sectorSeleccionado)[event.target.value]);
-    
-    // console.log(pintarTituloPorIndicador(worldbank.tituloPorIndicador(WORLDBANK,paisSeleccionado,sectorSeleccionado)[event.target.value]));
-    // return indicadoresData
-    // tablaDatos.innerHTML =
-    //console.log(worldbank.datosPaisSector(WORLDBANK,paisSeleccionado,sectorSeleccionado));
-    //  WORLDBANK.PER.indicators[event.target.id].data
-    //  console.log(selectRangoYear(numeroInicial.value, numeroFinal.value, WORLDBANK.PER.indicators[event.target.id].data))
+    let indicador = worldbank.tituloDeIndicadorporIndice(WORLDBANK, paisSeleccionado, sectorSeleccionado, indice);
+    let stringTitulo = `<h3 id="tituloIndicador"></h3>`;
+    stringTitulo = `${indicador}`
+    tituloIndicador.innerHTML = stringTitulo;
 });
+
+
+// console.log(pintarTituloPorIndicador(worldbank.tituloPorIndicador(WORLDBANK,paisSeleccionado,sectorSeleccionado)[event.target.value]));
+// return indicadoresData
+// tablaDatos.innerHTML =
+// console.log(worldbank.datosPaisSector(WORLDBANK,paisSeleccionado,sectorSeleccionado));
+//  WORLDBANK.PER.indicators[event.target.id].data
+//  console.log(selectRangoYear(numeroInicial.value, numeroFinal.value, WORLDBANK.PER.indicators[event.target.id].data))
+
+
 //muestra el grafico utilizando los datos en arrays
 // const grafico=()=>{
 //     let arrayGrafico= new Array(Object.entries( selectIndicador));
@@ -174,32 +166,43 @@ mostrarIndicador.addEventListener('click', (event) => {
 //   }
 
 //funcion ordenar datos del indicador ascendente y descendente
-let nuevoArrayPorcentaje=[]
-ordenar.addEventListener('change',(event)=>{
-    let ordenSeleccionado = event.target.value;
-    console.log(ordenSeleccionado);
-    for(let i=0;i< ordenSeleccionado.length; i++){
-        let resultadOrden = worldbank.ordenarDatoIndicador(years,ordenSeleccionado[i]);   
-        console.log(worldbank.ordenarDatoIndicador(years,ordenSeleccionado[i])); 
+// let nuevoArrayPorcentaje=[]
+// ordenar.addEventListener('change',(event)=>{
+//     let ordenSeleccionado = event.target.value;
+//     //console.log(ordenSeleccionado);
+//     for(let i=0;i< ordenSeleccionado.length; i++){
+//         let resultadOrden = worldbank.ordenarDatoIndicador(years,ordenSeleccionado[i]);   
+//         console.log(worldbank.ordenarDatoIndicador(years,ordenSeleccionado[i])); 
+//     }
+//     return ordenSeleccionado;
+//     // if (ordenSeleccionado ==='ascendente')
+// })
+//----------------------------
+
+ordenar.addEventListener('click', (event) => {
+    let ordenSeleccionado=event.target.value;
+    if(ordenSeleccionado==='ascendente'){
+        console.log(window.worldbank.formulaOrdenar(years));
+    }else{
+        console.log(window.worldbank.formulaOrdenar(years).reverse());
     }
-    return ordenSeleccionado;
-    // if (ordenSeleccionado ==='ascendente')
+
 })
+
+//------------------------------------
 
 //evento click para mostrar el promedio de los porcentajes 
 promedio.addEventListener('click', (event) => {
     event.preventDefault();
     let datoPromedio = [];
-    for(let i=0; i<porcentaje.length;i++){
-        if(porcentaje[i]!== ''){
+    for (let i = 0; i < porcentaje.length; i++) {
+        if (porcentaje[i] !== '') {
             datoPromedio.push(Number(porcentaje[i]))
-            // let porcentajeTotal = new Set([porcentaje[i].toFixed(2)]);
-            // let concatPorcentaje = Array.from(porcentajeTotal);
-            // console.log(concatPorcentaje);
         }
     }
-    let resultadoPromedio = window.worldbank.formulaPromedio(datoPromedio);
-    muestraPromedio.innerHTML='EL PROMEDIO ES: ' + resultadoPromedio.toFixed(2);
+    let resultadoPromedio = worldbank.formulaPromedio(datoPromedio);
+    muestraPromedio.innerHTML = 'EL PROMEDIO ES: ' + resultadoPromedio.toFixed(2);
+    return datoPromedio;
 })
 //evento al boton buscar que permite mostrar el grafico al dar click
 buscar.addEventListener('click', (event) => {
