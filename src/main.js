142edb1199183284c62f4b882ece6220a7d69f2f
mi19her <milcaph.19@gmail.com>
2019-07-18 23:09:13 -0500

    areglos de estilo en css para la quint vista

--- src/main.js
+++ src/main.js
@@ -40,20 +40,20 @@ menuSectores.addEventListener('click', () => {
 });
 // evento click al boton ingresar, para validar contraseña
 btnIngresar.addEventListener('click', () => {
-  if (password.value === '') {
-    mnsInc.innerHTML = 'ingrese password';
-  }
-  else {
-    if (password.value === 'laboratoria') {
+  // if (password.value === '') {
+  //   mnsInc.innerHTML = 'ingrese password';
+  // }
+  // else {
+  //   if (password.value === 'laboratoria') {
       document.getElementById('header').classList.remove('hide');
       document.getElementById('seccion1').classList.add('hide');
       document.getElementById('seccion2').classList.remove('hide');
-    }
-    else {
-      mnsInc.innerHTML = 'contraseña incorrecta';
-    }
-  }
-  password.value = '';
+  //   }
+  //   else {
+  //     mnsInc.innerHTML = 'contraseña incorrecta';
+  //   }
+  // }
+  // password.value = '';
 });
 // funcion para validar la seleccion de una bandera(Pais) y muestra el titulo del pais y sus indicadores en el select
 let paisSeleccionado;
@@ -94,8 +94,9 @@ const pintarDatosxSectorxIndicadores = (obj) => {
   let stringDatosSector = ' ';
   porcentaje = Object.values(obj);
   let i;
-  for (i in years) {
+  for (i in obj) {
     if (years[i] !== ' ') {
+      console.log(years[i]);
       stringDatosSector +=
         ` <tr><td> ${i} </td>
             <td>${years[i].toFixed(2)}</td></tr>`;
@@ -105,9 +106,8 @@ const pintarDatosxSectorxIndicadores = (obj) => {
 };
 // funcion para mostrar los datos del indicador seleccionado
 mostrarIndicador.addEventListener('click', (event) => {
+  mostrarDatosSector.innerHTML = ' ';
   let indice = event.target.id;
-  console.log(indice);
-
   document.getElementById('seccion4').classList.add('hide');
   document.getElementById('seccion5').classList.remove('hide');
   const indicadoresData = worldbank.datosPaisSector(WORLDBANK, paisSeleccionado, sectorSeleccionado);
@@ -117,6 +117,7 @@ mostrarIndicador.addEventListener('click', (event) => {
   stringTitulo = `${indicador}`;
   tituloIndicador.innerHTML = stringTitulo;
   grafico();
+
 });
 // muestra el grafico utilizando los datos en arrays
 const grafico = () => {
